d703b0be5b6af3d81accb57d3420a0680ed3ef95
Taleen04 <taleen.kha2004@gmail.com>
2025-01-16 16:50:11 -0800

    add sequential-ids package and memory storage packge

--- controller/noteController.js
+++ controller/noteController.js
@@ -2,4 +2,18 @@ exports.getAllNotess = (req, res) => {
     res.send("get all notes...");
 }
 
+exports.saveNote = (req, res) => {
+    res.send("save note...");
+}
+
+
+exports.updateNote = (req, res) => {
+    res.send("update note...");
+}
+
+
+exports.deleteNote = (req, res) => {
+    res.send("delete note...");
+}
+
 
--- route/noteRoute.js
+++ route/noteRoute.js
@@ -4,6 +4,9 @@ var noteController=require('../controller/noteController')
 
 
 router.get('/notes',noteController.getAllNotes);
+router.post('/notes/save',noteController.saveNote);
+router.put('/notes/update',noteController.updateNote);
+router.delete('/notes/delete',noteController.deleteNote);
 
 
 module.exports = router;
